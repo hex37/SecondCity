a342800a0da0ff17362a72e6dc6bd706e16596c8
WilliamNelson37 <bwnelson93@gmail.com>
2025-08-15 00:10:45 -0500

    Adds Diablerist and Generation to the preferences page

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/darkpack_diablerist.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, type FeatureToggle } from '../base';
+
+export const diablerist: FeatureToggle = {
+  name: 'Is Diablerist',
+  description:
+	'If checked, You will have committed Diablerie in your lifetime.',
+  component: CheckboxInput,
+};
